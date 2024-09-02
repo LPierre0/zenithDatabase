0c36c5c2a07c4e0bd3255c2779773e026be564fd
Pierre Lafage <lafagepierre2210@gmail.com>
2024-09-02 13:01:38 +0200

    [Utils] : Scrapping of items to create database

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import json
 from time import sleep
 import bs4
 import requests
@@ -11,9 +12,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 import re
 import lxml
 
+LVL_MAX = 230
+
+def concat_list(list1, list2):
+    for item in list2:
+        if item not in list1:
+            list1.append(item)
+    return list1
 
 def get_driver(url):
     user_data_dir = "/home/pierre/.config/google-chrome"
@@ -27,11 +36,17 @@ def get_driver(url):
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-
     driver.maximize_window()
     driver.get(url)
+    driver.delete_all_cookies()
+    driver.execute_script("window.localStorage.clear();")
+    driver.execute_script("window.sessionStorage.clear();")
     driver.implicitly_wait(10) 
     return driver
+
+def get_html(driver):
+    sleep(1)
+    return driver.page_source
 
 def get_soup(html):
     return bs4.BeautifulSoup(html, 'lxml')
@@ -49,6 +64,16 @@ def remove_popups(driver):
     except TimeoutException:
         print("Pop-up not found or did not appear in the specified time frame.")
 
+def press_item_type(driver, item_type):
+    try:
+        item_type_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f"//button[contains(@class, 'MuiIconButton-root') and .//img[contains(@alt, '{item_type}')]]"))
+        )
+        item_type_button.click()
+        print(f"Bouton de type d'item {item_type} cliqué.")
+        sleep(1)
+    except TimeoutException:
+        print(f"Le bouton de type d'item {item_type} n'est pas apparu dans le délai spécifié.")
 
 def press_research(driver):
     try:
@@ -57,6 +82,7 @@ def press_research(driver):
         )
         search_button.click()
         print("Bouton de recherche cliqué.")
+        sleep(10)
     except TimeoutException:
         print("Le bouton de recherche n'est pas apparu dans le délai spécifié.")
 
@@ -71,49 +97,117 @@ def accept_cookies(driver):
     except TimeoutException:
         print("Le bouton d'autorisation n'est pas apparu dans le délai spécifié.")
 
-def change_level_item(driver, lvl_min = 0, lvl_max = 230):
-
-    first_thumb = driver.find_element(By.CSS_SELECTOR, '.MuiSlider-thumb[data-index="0"]')
-    second_thumb = driver.find_element(By.CSS_SELECTOR, '.MuiSlider-thumb[data-index="1"]')
-
-    value_first = first_thumb.get_attribute('aria-valuenow')
-    value_second = second_thumb.get_attribute('aria-valuenow')
-    print(value_first, value_second)
-
-    while int(value_first) != lvl_min:
-        value_moving = int((lvl_min - int(value_first)))
-        if value_moving == 1 or value_moving == -1:
-            value_moving = value_moving * 2
-        actions = ActionChains(driver)
-        actions.click_and_hold(first_thumb).move_by_offset(value_moving, 0).release().perform()
-        value_first = first_thumb.get_attribute('aria-valuenow')
-    sleep(3)
-    while int(value_second) != lvl_max:
-        value_moving = int((lvl_max - int(value_second)))
-        if value_moving == 1 or value_moving == -1:
-            value_moving = value_moving * 2
-        actions = ActionChains(driver)
-        actions.click_and_hold(second_thumb).move_by_offset(value_moving, 0).release().perform()
-        value_second = second_thumb.get_attribute('aria-valuenow')
+def get_item_name_and_rarity(block_item):
+    item = block_item.find('div', {"class" : "item-name"})
+    item_name = item.text
+    rarity = item["class"][1]
+    rarity = rarity.replace('-item', '')
+    return item_name, rarity
 
 
+def ajust_thumb(driver, thumb, nb_move, start_value, objective_value, sleep_time=0.02):
+    actions = ActionChains(driver)
+    print(f"Moving thumb from {start_value} to {objective_value}")
+    print(f"Nb move: {nb_move}")
+    if nb_move == 0:
+        return
+    actions.click(thumb).perform()
+    if nb_move > 0:
+        for i in range(0, nb_move):
+            actions.send_keys(Keys.ARROW_RIGHT).perform()
+            sleep(sleep_time)
+    else:
+        for i in range(-nb_move):
+            actions.send_keys(Keys.ARROW_LEFT).perform()
+            sleep(sleep_time)
+    if int(thumb.get_attribute('aria-valuenow')) != objective_value:
+        actions.send_keys(Keys.ARROW_RIGHT).perform()
+        new_start_value = int(thumb.get_attribute('aria-valuenow'))
+        ajust_thumb(driver, thumb, objective_value - new_start_value, new_start_value, objective_value, sleep_time = sleep_time * 2)
 
+def change_level_item(driver, lvl_min = 0, lvl_max = LVL_MAX):
+
+    first_thumb = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '.MuiSlider-thumb[data-index="0"]'))
+    )
+    second_thumb = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '.MuiSlider-thumb[data-index="1"]'))
+    )
+
+    value_first = int(first_thumb.get_attribute('aria-valuenow'))
+    value_second = int(second_thumb.get_attribute('aria-valuenow'))
+    print(f"Starting values: {value_first}, {value_second}, goal values: {lvl_min}, {lvl_max}")
+    
+
+    nb_move_first = lvl_min - value_first
+    nb_move_second = lvl_max - value_second
+    print(nb_move_first, nb_move_second)
+    ajust_thumb(driver, first_thumb, nb_move_first, value_first, lvl_min)
+    ajust_thumb(driver, second_thumb, nb_move_second, value_second, lvl_max)
+
+    print(f"Final values: {first_thumb.get_attribute('aria-valuenow')}, {second_thumb.get_attribute('aria-valuenow')}")
+
+
+
+def get_item_list_at_lvl(driver, type, lvl):
+    change_level_item(driver, 0, lvl)
+    press_research(driver)
+    html = get_html(driver)
+    soup = get_soup(html)
+    block_items = soup.find_all('div', {"class": "equipment w-full sm:w-1/2-gap-1 md:w-1/2-gap-1 lg:w-full-gap-1 xl:w-1/2-gap-1 2xl:w-2/6-gap-1 flex flex-col"})
+    dict = {}
+    last_lvl = 0
+    for block_item in block_items:
+        lvl = block_item.find('div', {"class" : "item-lvl"}).text
+        lvl = lvl.replace('Niv. ', '')
+        lvl = int(lvl)
+        link_to_wakfu = block_item.find('a', {"class" : "item-link"})['href']
+        item_name, rarity = get_item_name_and_rarity(block_item)
+        img = block_item.find('img', {"class" : "inner-image"})['src']
+        img = img.replace('..', 'https://www.zenithwakfu.com')
+        if link_to_wakfu not in dict:
+            dict[link_to_wakfu] = (lvl, item_name, rarity, img)
+            last_lvl = lvl
+    return dict, last_lvl
+
+def get_all_items(driver, type):
+    press_item_type(driver, type)
+    dict_items = {}
+    lvl = LVL_MAX
+    while lvl > 1:
+        dict_to_treat, lvl = get_item_list_at_lvl(driver, type, lvl)
+        dict_items.update(dict_to_treat) 
+        print(lvl)
+    press_item_type(driver, type)
+    return dict_items
+
+
+def save_dict_to_json(dict, filename):
+    with open (filename, 'w') as file:
+        json.dump(dict, file, indent=4, ensure_ascii=False)
+
+
+def test_slider():
+    driver = get_driver('https://www.zenithwakfu.com/builder/f265c')
+    change_level_item(driver, 0, 230)
+    change_level_item(driver, 0, 100)
+    change_level_item(driver, 0, 50)
+    change_level_item(driver, 0, 78)
+
+    sleep(5)
+    driver.quit()
 
 def main():
     url = 'https://www.zenithwakfu.com/builder/f265c'
     driver = get_driver(url)
-    change_level_item(driver, 141, 155)
-    press_research(driver)
-    sleep(5)
-
-    html = driver.page_source
-    soup = get_soup(html)
-    items = soup.find_all('a', {"class": "item-link"})
-    for item in items:
-        print(item['href'])
-    driver.quit()
+    #list_type = ['Casque', 'Amulette', 'Anneau', 'Plastron', 'Dague', 'Armes 1 Main', 'Armes 2 Mains', 'Ceinture', 'Bottes', 'Cape', 'Bouclier', 'Emblème']
+    list_type = ['Amulette']
+    for type in list_type:
+        dict_item = get_all_items(driver, type)
+        save_dict_to_json(dict_item, f'json/{type}.json')
 
 
 
 if __name__ == '__main__':
     main()
+    
