66a398ca2eb843d4280c06182944a35573b946ed
Pierre Lafage <lafagepierre2210@gmail.com>
2024-09-11 14:00:38 +0200

    [SQL] : Insert build

--- src/sql/sql_insert.py
+++ src/sql/sql_insert.py
@@ -28,7 +28,7 @@ def add_all_json_items(json_file, item_type, conn, cursor):
         item = items[link]
         add_item(link, item[2], item[3], item[1], item[0], item_type, conn, cursor)
 
-def add_all_json():
+def add_all__items_json():
     conn = sqlite3.connect('db/zenithDatabase.sqlite')
     cursor = conn.cursor()
 
@@ -38,5 +38,31 @@ def add_all_json():
 
     conn.close()
 
-if __name__ == '__main__':
-    add_all_json()
+def add_build_dictionnary(build_dict):
+    conn = sqlite3.connect('db/zenithDatabase.sqlite')
+    cursor = conn.cursor()
+    nb_builds_total = len(build_dict)
+    nb_builds_inserted = 0
+    for link in build_dict:
+        build = build_dict[link]
+        items = build['items']
+        print(items)
+        try:
+            cursor.execute('''
+                INSERT INTO build (url, lvl, name, creation_date, classe, casque_url, amulette_url, plastron_url, anneau1_url, anneau2_url, bottes_url, cape_url, epaulettes_url, ceinture_url, dague_or_bouclier_or_armes_url, armes_url, embleme_url)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (link, build['lvl'], build['name'], build['date'], build['classe'], items['Casque'], items['Amulette'], items['Plastron'], items['Anneau1'], items['Anneau2'], items['Bottes'], items['Cape'], items['Epaulettes'], items['Ceinture'], items['DagueOrBouclierOrArmes'], items['Armes'], items['Embleme']))
+
+            conn.commit()
+            nb_builds_inserted += 1
+            print(f"Build {link} inserted. {nb_builds_inserted} builds inserted out of {nb_builds_total}")
+
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e} with the build {link}")
+            break
+    conn.close()
+    return
+
+
+if __name__ == "__main__":
+    add_build_dictionnary(get_json('json/builds.json'))
